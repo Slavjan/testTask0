e19de151048f2cbb873186f01e1a4fc27e9dcab2
Slavjan <khaidayev@gmail.com>
2017-11-27 02:06:41 +0700

    restructuring

--- main.cpp
+++ main.cpp
@@ -1,8 +1,39 @@
-#include <QCoreApplication>
+#include <string>
 
-int main(int argc, char *argv[])
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "node.h"
+#include "serializer.h"
+
+int main(int argc, char **argv)
 {
-  QCoreApplication a(argc, argv);
+  Node *tree = nullptr;
 
-  return a.exec();
+  const char *opts = "i:o:";
+
+  int opt;
+  while((opt = getopt(argc, argv, opts)) != -1) {
+    switch(opt){
+    case 'i':
+      tree = Serializer::deserialize( optarg );
+      break;
+
+    case 'o':
+      if( !tree ){
+        std::cerr << " nothing to serialize! " << std::endl;
+        return 2;
+      }
+      Serializer::serialize( tree, optarg );
+      break;
+
+    default:
+      std::cerr << " unexpected argument " << std::endl;
+      return 1;
+    }
+  }
+
+  tree->print();
+  return 0;
 }
